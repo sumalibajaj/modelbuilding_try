50ca8e4ac2e425a855511b17ddb288ba03e5b14e
Sumali Bajaj <34308572+sumalibajaj@users.noreply.github.com>
2021-03-29 15:50:55 +0100

    initial package setup

---
+++ R/fbind.R
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @param a
+#' @param b
+#'
+#' @return
+#' @export
+#'
+#' @examples
+fbind <- function(a, b) {
+  factor(c(as.character(a), as.character(b)))
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(modelbuilding)
+
+test_check("modelbuilding")
